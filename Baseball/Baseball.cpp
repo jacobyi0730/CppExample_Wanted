42df288b6ec34a57032c39e7969ac935c60c628d
f11m4at4 <f11m4at4@gmail.com>
2025-01-06 12:06:01 +0900

    .

--- Baseball/Baseball.cpp
+++ Baseball/Baseball.cpp
@@ -24,7 +24,6 @@ int main()
 
         int strike = 0;
         int ball = 0;
-        bool isComplete = true;
         for (int i = 0; i < 3; i++)
         {
             for (int j = 0; j < 3; j++)
