642770211dceea15918507a2af84be20b3f7dab5
f11m4at4 <f11m4at4@gmail.com>
2025-01-06 11:58:42 +0900

    숫자야구

---
+++ Baseball/Baseball.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    // 1. 컴퓨터가 랜덤으로 1~9까지 중복되지 않는 숫자 3개를 기억하고싶다.
+    int quiz[3]{ 3, 6, 9 };
+    int answers[3]{ 0 };
+
+    while (true)
+    {
+        //int number = 19990120;
+        //int 년 = number / 10000;
+        //int 월 = (number / 100) % 100;
+        //int 일 = number % 100;
+        //cout << 년 << " " << 월 << " " << 일 << endl;
+        // 사용자의 입력 숫자3개
+        int answer;
+        cin >> answer; //123
+        answers[0] = (answer / 100) % 10;
+        answers[1] = (answer / 10) % 10;
+        answers[2] = answer % 10;
+        cout << answer << endl;
+
+        int strike = 0;
+        int ball = 0;
+        bool isComplete = true;
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                // 대답의 항목이 quiz 배열에 포함되어 있는가?
+                if (answers[i] == quiz[j])
+                {
+                    // 자릿수가 같으면 스트라잌 다르면 볼
+                    if (i == j)
+                        strike++;
+                    else
+                        ball++;
+                }
+            }
+        }
+        // strike과 ball을 더해서 0이라면 아웃이다.
+        if (strike + ball == 0)
+        {
+            // 아웃
+            cout << answer << " 아웃" << endl;
+        }
+        else if (strike == 3)
+        {
+            cout << strike << "strike 성공" << endl;
+            break;
+        }
+        else {
+            cout << strike << "strike, " << ball << "ball" << endl;
+        }
+
+        // 2. 사용자의 입력 숫자3개
+        // 3. 입력받은 숫자가 기억하고 있는 숫자를 비교해서 결과를 출력
+        //   3.1 성공 : 입,기 숫자가 완전동일
+        //   3.2 실패 : A : 아웃(숫자 3개가 다 다르면)
+        //              B : 하나라도 같으면 결과 출력
+    }
+}
--- EnumExample/EnumExample.cpp
+++ EnumExample/EnumExample.cpp
@@ -2,6 +2,7 @@
 //
 
 #include <stdio.h>
+#include <iostream>
 //
 //namespace EnemyState {
 //	enum EEnemyState {
@@ -45,40 +46,49 @@
 //	}
 //}
 
+//
+//// 기본 매크로 기능(텍스트를 치환)
+//#define MAX 100
+//#define MY_MAX int a
+//// 매크로 함수
+//#define ADD(a,b) ((a)*(b))
+//#define PRINT(str) printf(#str);
+//#define PRINT2(a,b)	printf(a##b);
+//
+//#define CHEAT_VERSION
+////#undef CHEAT_VERSION
+//
+//int main2()
+//{
+//
+////	int gold = 0;
+////#ifdef CHEAT_VERSION
+////	gold = 1000000;
+////#else
+////	gold = 100;
+////#endif
+////
+////	PRINT2("Hello", " World!");
+//	//PRINT("Hello World!!\n");
+//	//int result = ADD(10, 2+3 );
+//	//printf("%d", result);
+//}
 
-// 기본 매크로 기능(텍스트를 치환)
-#define MAX 100
-#define MY_MAX int a
-// 매크로 함수
-#define ADD(a,b) ((a)*(b))
-#define PRINT(str) printf(#str);
-#define PRINT2(a,b)	printf(a##b);
-
-#define CHEAT_VERSION
-//#undef CHEAT_VERSION
 
 int main()
 {
-	int gold = 0;
-#ifdef CHEAT_VERSION
-	gold = 1000000;
-#else
-	gold = 100;
-#endif
-
-	PRINT2("Hello", " World!");
-	//PRINT("Hello World!!\n");
-	//int result = ADD(10, 2+3 );
-	//printf("%d", result);
+	// 별을 찍어봅시다.
+	// 숫자를 하나 입력받고
+	int count;
+	std::cin >> count;
+	// 그 숫자만큼 행을 만들고
+	for (int i = 0; i < count; i++)
+	{
+		// 위에서부터 아래로 1개씩 *표시가 늘어나도록
+		for (int j = 0; j < i + 1; j++)
+		{
+			std::cout << "*";
+		}
+		std::cout << std::endl;
+	}
 }
-
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
-
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
